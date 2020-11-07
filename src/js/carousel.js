64d100c46de0958501db5e4f2a89f6ba75408b6a
vinocrzy <vinoth.vinothkanna.kanna2@gmail.com>
2020-11-07 14:14:17 +0530

    half layout done

---
+++ src/js/carousel.js
@@ -0,0 +1,8 @@
+import "owl.carousel";
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 3,
+    dots: true,
+  });
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,3 @@
+import "owl.carousel/dist/assets/owl.carousel.css";
 import "./../scss/style.scss";
+import "./carousel";
