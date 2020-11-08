70727e055ad182da8ca2d23f997e6d43e5808a7f
vinocrzy <vinoth.vinothkanna.kanna2@gmail.com>
2020-11-08 20:15:18 +0530

    fine tune

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,15 @@
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "./../scss/style.scss";
 import "./carousel";
+
+$(document).ready(function () {
+  $(".pop-close").click(function () {
+    $(".alert-strip").addClass("hide");
+  });
+  $(".burger-menu").click(function () {
+    $(".head-menu-nav").addClass("open");
+  });
+  $(".head-menu-nav").click(function () {
+    $(".head-menu-nav").removeClass("open");
+  });
+});
